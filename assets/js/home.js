58df7cb5dc1b529f34b9446443b7b856eaa58360
Chengyu Dai <aacherythdai@gmail.com>
2021-04-29 13:40:16 -0700

    commit on 'homepage.cd'

---
+++ assets/js/home.js
@@ -0,0 +1,13 @@
+var footerEl = document.getElementById("page-footer");
+
+function openNav() {
+    document.getElementById("nav-footer").style.height="100%";
+}
+
+function showfooter() {
+    footerEl.classList.remove("hidden");
+}
+
+function closeNav() {
+    document.getElementById("nav-footer").style.height="0";
+}
