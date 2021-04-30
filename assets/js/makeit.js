f0e2cd56065f74e8768a51c173844b0879bdfac0
Omri <omri.ronen4@gmail.com>
2021-04-29 18:35:45 -0700

    final draft

--- assets/js/makeit.js
+++ assets/js/makeit.js
@@ -1,10 +1,16 @@
+var allFavoriteInfo = [];
+var idArray = [];
+
 // side nav 
 $(document).ready(function(){
     $('.sidenav').sidenav();
   });
 
 init();
-
+$("form").on('submit', function(e){
+  e.preventDefault()
+  searchHandler(e)
+})
 // on the click of the magnify glass search button, searchRecipe function runs.
   $("#search-btn").on("click", searchRecipe)
 
@@ -14,22 +20,17 @@ init();
 
     var search = $("#search-input").val().trim();
 
-
     if(search){
     
       // clear the input field
       $("#search-input").val("");
-      
-      // save search to local storage
-      // saveSearch(search);
       
       // runs the doSearch functions given the "search" parameter
       // this function will initiate the search for recipes 
       doSearch(search);
     }
   }
-
-
+  
   // this function will initiate the search for recipes 
 function doSearch(search){
 
@@ -39,47 +40,30 @@ function doSearch(search){
   $.ajax({
     contentType: "application/json",
     method: "GET",
-    url: `https://api.spoonacular.com/recipes/findByIngredients?ingredients=${search}&number=10&ranking=1&apiKey=ec05068cf090412eb77ead2a99d031e9`,
+    url: `https://api.spoonacular.com/recipes/complexSearch?includeIngredients=${search}&number=10&apiKey=ec05068cf090412eb77ead2a99d031e9&addRecipeInformation=true&fillIngredients=true&query`,
   }).then(function(data){
     console.log(data);
     renderSearch(data);
-
-    for (var i=0; i < data.length; i++){
-      allRecipeInfo = data[i]
-      console.log(allRecipesInfo.id)
-    }
     
-    // getRecipeLinkById(allRecipesInfo.id)
   })
 }
 
-
-
-
-// function getRecipeLinkById(id){
-//   $.ajax({
-//     contentType: "application/json",
-//     method: "GET",
-//     url: `https://api.spoonacular.com/recipes/${id}/information?includeNutrition=false&apiKey=ec05068cf090412eb77ead2a99d031e9`,
-//   }).then(function(data){
-//     console.log(data);
-//   })
-// }
-
-
-
-
-
-
-
 // rendering search results
 function renderSearch(data) {
+  
   $("#ingredients-container").show();
+  $("#favorites-container").hide();
 
-// create restaurant cards for each restaurant
-for (var i=0; i < data.length; i++) {
+var allRecipeInfo = data.results;
+console.log(allRecipeInfo)
+
+// 
+for (var i=0; i < allRecipeInfo.length; i++) {
    
-  allRecipesInfo = data[i]
+var recipesInfo = allRecipeInfo[i]
+
+// idArray.push(recipeIds);
+// localStorage.setItem("recipeIds", JSON.stringify(idArray));
 
 var cardContainer = $("<div>");
 cardContainer.addClass("col m12 l6");
@@ -97,7 +81,7 @@ var card = $("<div>");
 
     var jpg = $("<img>");
     jpg.attr("id", "recipe-img");
-    jpg.attr("src", `${allRecipesInfo.image}`)
+    jpg.attr("src", `${recipesInfo.image}`)
     cardImg.append(jpg);
 
     var cardContent = $("<div>");
@@ -108,118 +92,164 @@ var card = $("<div>");
     var name = $("<div>");
     name.attr("id", "recipe-name");
     name.addClass("card-title");
-    name.text(`${allRecipesInfo.title}`);
+    name.text(`${recipesInfo.title}`);
     cardContent.append(name);
 
     var missedIngredientCount = $("<div>");
     missedIngredientCount.attr("id", "missed-ingredient-count");
-    missedIngredientCount.text(`Missed Ingredients: ${allRecipesInfo.missedIngredientCount}`);
+    missedIngredientCount.text(`Missing Ingredients: ${recipesInfo.missedIngredientCount}`);
     cardContent.append(missedIngredientCount);
 
-    // var missedIngredientsList = $("<div>");
-    // missedIngredientsList.attr("id", "missed-ingredient-list");
-    // missedIngredientsList.text(`Missed Ingredients: ${allRecipesInfo.missedIngredients[0].name}`);
-    // cardContent.append(missedIngredientsList);
+    var linkBtn = $("<button>");
+    linkBtn.attr("id", "link-btn");
+    linkBtn.addClass("btn btn-small btn-color left");
+    linkBtn.attr("type", "button");
+    linkBtn.html(`<i class=" material-icons">link</i>`);
+    // cardContent.append(linkBtn);
+    
+    var link = $("<a>");
+    link.attr("href", recipesInfo.sourceUrl);
+    link.attr("target", "_blank");
+    link.append(linkBtn);
+    cardContent.append(link);
 
-    cardContent.on("click", function(){
-      
-    })
-    // var price = $("<div>");
-    // price.attr("id", "restaurant-price");
-    // price.text(`Price: ${businessInfo.price}`);
-    // cardContent.append(price);
+    var favoriteBtn = $("<button>");
+    favoriteBtn.attr("id", "favorite-btn");
+    favoriteBtn.addClass("btn btn-small btn-color left");
+    favoriteBtn.attr("type", "button");
+    favoriteBtn.html(`<i class=" material-icons">favorite_border</i>`);
+    favoriteBtn.attr("data-image", recipesInfo.image);
+    cardContent.append(favoriteBtn);
 
-    // var rating = $("<div>");
-    // rating.attr("id", "restaurant-rating");
-    // rating.text(`Yelp Rating: ${businessInfo.rating}`);
-    // cardContent.append(rating);
   }
-
-
-
-
 }
 
 
-// render search results 
+// select favorite restaurant to save
+$("#render-search").on("click", "#favorite-btn", saveFavoriteHandler);
 
-// function renderSearch(data) {
+function saveFavoriteHandler(event){
+  event.stopPropagation();
+  
+  // change favorite icon to signify addition to favorites
+  $(this).html(`<i class=" material-icons">favorite</i>`);
 
-//   $("#ingrediets-container").show();
+  var favoriteImg = $(this).attr("data-image");
+  var favoriteName = $(this).siblings("#recipe-name").text();
+  // var favoritePrice = $(this).siblings("#restaurant-price").text();
+  // var favoriteRating = $(this).siblings("#restaurant-rating").text();
+  // var favoriteLink = $(this).siblings("#restaurant-link").text();
 
-//   var allIngredientsInfo = data.usedIngredients;
-
-
-//   //create restaurant cards for each restaurant 
-
-//   for(var i = 0; i < allIngredientsInfo; i++) {
-
-//     var cardContainer = $("<div>");
-//     cardContainer.addClass("col m12 l6");
-//     $("#render-search").append(cardContainer);
-
-//     var card = $("<div>");
-//     card.attr("id", "restaurant-card");
-//     card.addClass("card horizontal hoverable");
-//     cardContainer.append(card);
-
-//     var cardImg = $("<div>");
-//     cardImg.addClass("card-image");
-//     card.append(cardImg);
-
-//     var img = $("<img>");
-//     img.attr("id", "restaurant-img");
-//     img.attr("src", `${ADD}`);
-//     cardImg.append(img);
-
-//     var cardContent = $("<div>");
-//     cardContent.addClass("card-content");
-//     card.append(cardContent);
+  var favoriteInfo = {
+    imgUrl: favoriteImg,
+    name: favoriteName,
+    // price: favoritePrice,
+    // rating: favoriteRating,
+    // link: favoriteLink
+  }
+  
+  // checks for duplicate entries
+  if(!allFavoriteInfo.includes(favoriteInfo)){
     
-//     var name = $("<div>");
-//     name.attr("id", "restaurant-name");
-//     name.addClass("card-title");
-//     name.text(`${ADD}`);
-//     cardContent.append(name);
-//     // change price to something
-//     var price = $("<div>");
-//     price.attr("id", "restaurant-price");
-//     price.text(`Price: ${ADD}`);
-//     cardContent.append(price);
-//     // change rating to something 
-//     var rating = $("<div>");
-//     rating.attr("id", "restaurant-rating");
-//     rating.text(`Yelp Rating: ${ADD}`);
-//     cardContent.append(rating);
-//   }
-
-// }
+    // add city to search history and save to localStorage
+    allFavoriteInfo.push(favoriteInfo);
+    localStorage.setItem("allFavorites", JSON.stringify(allFavoriteInfo));
+  }
+  
+  // console.log(favoriteInfo);
+  console.log(allFavoriteInfo);
+}
 
 
+$("#all-favorites-btn").on("click", viewFavoritesHandler);
 
+function viewFavoritesHandler(event){
+  event.stopPropagation();
 
+  $("#ingredients-container").hide();
+  $("#favorites-container").show();
 
+  // get stored favorites from localStorage
+  var savedFavorites = JSON.parse(localStorage.getItem("allFavorites"));
+
+  // checks for data in localStorage
+  if(savedFavorites !== null){
+    allFavoriteInfo = savedFavorites;
+  }else{
+    allFavoriteInfo = [];
+  }
+
+  // create cards for each favorite restaurant 
+  for(var i = 0; i < allFavoriteInfo.length; i++){
+
+    var favorite = allFavoriteInfo[i];
+    
+    // create restaurant card
+    var cardContainer = $("<div>");
+    cardContainer.addClass("col s12 m12 l6");
+    $("#render-favorites").append(cardContainer);
+    
+    var card = $("<div>");
+    card.attr("id", "restaurant-card");
+    card.addClass("card horizontal hoverable");
+    cardContainer.append(card);
+    
+    var cardImg = $("<div>");
+    cardImg.attr("id", "image-container");
+    cardImg.addClass("card-image");
+    card.append(cardImg);
+
+    var img = $("<img>");
+    img.attr("id", "restaurant-img");
+    img.attr("src", favorite.imgUrl);
+    cardImg.append(img);
+
+    var cardContent = $("<div>");
+    cardContent.attr("id", "content-container");
+    cardContent.addClass("card-content");
+    card.append(cardContent);
+    
+    var name = $("<h4>");
+    name.attr("id", "restaurant-name");
+    name.addClass("card-title");
+    name.text(favorite.name);
+    cardContent.append(name);
+
+    var price = $("<h6>");
+    price.attr("id", "restaurant-price");
+    price.text(`Price: ${favorite.price}`);
+    cardContent.append(price);
+    
+    var rating = $("<h6>");
+    rating.attr("id", "restaurant-rating");
+    rating.text(`Rating: ${favorite.rating}`);
+    cardContent.append(rating);
+
+    var linkBtn = $("<button>");
+    linkBtn.attr("id", "link-btn");
+    linkBtn.addClass("btn btn-small btn-color left");
+    linkBtn.attr("type", "button");
+    linkBtn.html(`<i class=" material-icons">link</i>`);
+    cardContent.append(linkBtn);
+    
+    var link = $("<a>");
+    link.attr("href", favorite.link);
+    linkBtn.append(link);
+
+    // var favoriteBtn = $("<button>");
+    // favoriteBtn.attr("id", "favorite-btn");
+    // favoriteBtn.addClass("btn btn-small btn-color left");
+    // favoriteBtn.attr("type", "button");
+    // favoriteBtn.html(`<i class=" material-icons">favorite_border</i>`);
+    // cardContent.append(favoriteBtn);
+  }
+}
 
 // initialize page
 function init(){
   $("#ingredients-container").hide();
+  $("#favorites-container").hide();
 }
-//   $("button").on("click", function () {
-//     var searchCity = $("#search").val();
-//     fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${searchCity}&units=imperial&appid=2f13e6ddf4fe6dc7bcf87d5d56fa266c`)
-//         .then(function (data) {
-//             return data.json();
-//         })
-//         .then(function (data) {
-//             console.log(data)
-//             console.log('temp', data.list[0].main.temp);
-//             console.log('humidity', data.list[1].main.humidity);
-//             console.log('speed', data.list[0].wind.speed);
-//             console.log('coords', data.city.coord);
-//             var lat = data.city.coord.lat;
-//             var lon = data.city.coord.lon;
-//         })
-// })
 
 
 
