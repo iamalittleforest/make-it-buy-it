efd149c0ce4724701d23f72d8f3a92fc94910f62
Chengyu Dai <aacherythdai@gmail.com>
2021-04-30 15:07:53 -0700

    commit on 'homepage.cd'

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # project-1
-Project 1
+Make It | Buy It
+
+
 
 Use the following naming convention for new branches "feature_initials".
--- assets/js/home.js
+++ assets/js/home.js
@@ -4,10 +4,6 @@ function openNav() {
     document.getElementById("nav-footer").style.height="100%";
 }
 
-function showfooter() {
-    footerEl.classList.remove("hidden");
-}
-
 function closeNav() {
     document.getElementById("nav-footer").style.height="0";
 }
